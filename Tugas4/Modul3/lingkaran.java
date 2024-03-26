8c0430e2847c47e790f07ea1392b3ae7994657fa
zahrakuvitaaberlin <160576191+zahrakuvitaaberlin@users.noreply.github.com>
2024-03-26 22:55:56 +0700

    Create lingkaran.java

---
+++ Tugas4/Modul3/lingkaran.java
@@ -0,0 +1,46 @@
+public class lingkaran{
+    int alas, tinggi;
+
+
+    public lingkaran(int alas){
+        this.alas = alas;
+    }
+    public lingkaran(String alas, String tinggi){
+        this.alas = Parsing(alas);
+        this.tinggi = Parsing(tinggi);     
+    }
+    public int Parsing(String a){
+        return Integer.parseInt(a);
+    }
+
+    public lingkaran(int alas, int tinggi){
+        this.alas = alas;
+        this.tinggi = tinggi;
+    }
+
+    public void setAlas(int alas){
+        this.alas = alas;
+    }
+
+    public void setTinggi(int tinggi){
+        this.tinggi = tinggi;
+    }
+
+    public int getAlas(){
+        return alas;
+    }
+
+    public int getTinggi(){
+        return tinggi;
+    }
+
+    public double hitungLuas(){
+        double hasil = (double)(getTinggi()*getAlas())/2;
+        return hasil;
+    }
+
+    public void displayMessage(){
+        System.out.println("Hitung Luas : "+hitungLuas());
+    }
+
+}
