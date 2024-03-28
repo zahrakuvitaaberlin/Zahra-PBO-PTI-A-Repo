998ee13b5cbb857969d64d118e39a1cc6f99f999
zahrakuvitaaberlin <160576191+zahrakuvitaaberlin@users.noreply.github.com>
2024-03-28 17:23:31 +0700

    Create Student.java

---
+++ Tugas5/TugasPercobaan/Student.java
@@ -0,0 +1,19 @@
+package TugasPBO.TugasPraktikkum;
+
+public class Student {
+    private String name;
+    private int mark;
+    public void setName(String n){
+    name=n;
+    }
+    public String getName(){
+    return name;
+    }
+    public void setMark(int m){
+    mark=m;
+    }
+    public int getMark(){
+    return mark;
+    }
+}
+
