8ebbcfd1f91fa2410fe8a0dcea71450933fd3fc7
zahrakuvitaaberlin <160576191+zahrakuvitaaberlin@users.noreply.github.com>
2024-03-28 17:25:55 +0700

    Create Vehicle1.java

---
+++ Tugas5/TugasPercobaan/Vehicle1.java
@@ -0,0 +1,25 @@
+package TugasPBO.TugasPraktikkum;
+
+public class Vehicle1{
+    public double load, maxLoad;
+    public Vehicle1 (double max){
+        this.maxLoad = max;
+    }
+    public double getLoad(){
+        return this.load;
+    }
+    public double getMaxLoad(){
+        return this.maxLoad;
+    }
+    public boolean addBox(double weight){
+        double temp = 0.0D;
+        temp = this.load + weight;
+        if(temp <= maxLoad){
+            this.load = this.load + weight;
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
