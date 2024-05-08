3ba40199d4f0f0bc3c2ac00011dfbdae00350195
zahrakuvitaaberlin <160576191+zahrakuvitaaberlin@users.noreply.github.com>
2024-05-08 22:04:03 +0700

    Create HourlyEmployee.java

---
+++ Tugas 7/src/Percobaan/HourlyEmployee.java
@@ -0,0 +1,33 @@
+package Percobaan;
+public class HourlyEmployee extends Employee {
+    private double wage; //upah per jam
+    private double hours; //jumlah jam tiap minggu
+    public HourlyEmployee(String name, String noKTP,
+    double hourlyWage, double hoursWorked) {
+    super(name, noKTP);
+    setWage(hourlyWage);
+    setHours(hoursWorked);
+    }
+    public void setWage(double hourlyWage){
+    wage = hourlyWage;
+    }
+    public double getWage(){
+    return wage;
+    }
+    public void setHours(double hoursWorked){
+    hours = hoursWorked;
+    }
+    public double getHours(){
+    return hours;
+    }
+    public double earnings(){
+    if(getHours()<=40)
+    return getWage() * getHours();
+    else
+    return 40 * getWage() + ( getHours()-40) *
+   getWage() * 1.5;
+    }
+    public String toString(){
+    return String.format("Hourly employee:"+super.toString()+"\nhourly wage"+getWage()+"\nhours worked:"+getHours());
+    }
+   }
